a8f5ee8bdfb68d53f7185e6eb4ae43fc89751680
Hyojin <dlgywls1130@gmail.net>
2022-06-19 21:21:41 +0900

    signup board

---
+++ js/main.js
@@ -0,0 +1,8 @@
+if (window.navigator.userAgent.indexOf("Edge") !== -1 || navigator.appVersion.indexOf("MSIE 10") !== -1 || window.navigator.userAgent.indexOf("Trident/7.0") > 0) { 
+    document.documentElement.className += ' crappy-browser';
+} else {
+    document.documentElement.className += ' modern-browser';
+}
+
+
+
