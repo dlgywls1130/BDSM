9104b4be468236042fe7d1c357701efb3bef6d63
Hyojin <dlgywls1130@gmail.net>
2022-06-23 00:35:06 +0900

    signup edit

--- js/main.js
+++ js/main.js
@@ -4,5 +4,39 @@ if (window.navigator.userAgent.indexOf("Edge") !== -1 || navigator.appVersion.in
     document.documentElement.className += ' modern-browser';
 }
 
+// selectbox
 
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+        
+
+
+  var agreeBtn = document.getElementsByClassName("agree-btn");
+
+  function handleClick(event) {
+    console.log(event.target);
+    // console.log(this);
+    // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+    console.log(event.target.classList);
+
+    if (event.target.classList[1] === "clicked") {
+      event.target.classList.remove("clicked");
+    } else {
+      for (var i = 0; i < agreeBtn.length; i++) {
+        agreeBtn[i].classList.remove("clicked");
+      }
+
+      event.target.classList.add("clicked");
+    }
+  }
+
+  function init() {
+    for (var i = 0; i < agreeBtn.length; i++) {
+        agreeBtn[i].addEventListener("click", handleClick);
+    }
+  }
+
+  init();
 
